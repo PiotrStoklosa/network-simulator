c59852732a6516ec377a3d8565053eb2b32d0ecb
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-04-10 15:01:52 +0200

    Initial commit

---
+++ src/main/java/com/nokia/uwr/NetworkSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.uwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkSimulatorApplication.class, args);
+	}
+
+}
