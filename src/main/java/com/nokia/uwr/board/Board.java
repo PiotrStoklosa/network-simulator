4b9bb0bd1db8e6c0c45cd2f2a4195f38de7e7f67
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-04-10 16:32:09 +0200

    Create project basics part II

--- src/main/java/com/nokia/uwr/board/Board.java
+++ src/main/java/com/nokia/uwr/board/Board.java
@@ -2,6 +2,8 @@ package com.nokia.uwr.board;
 
 import com.nokia.uwr.model.BTS;
 import com.nokia.uwr.model.UE;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,11 +11,13 @@ import java.util.Map;
 @Component
 public class Board {
 
+    private static final Logger LOGGER = LogManager.getLogger(Board.class);
+
     Map<UE, Localization> ue;
     Map<BTS, Localization> bts;
 
     public void addBts(){}
-    public void updateBts(){}
+
     public void addUe(){}
     public void updateUe(){}
     public void deleteUe(){}
