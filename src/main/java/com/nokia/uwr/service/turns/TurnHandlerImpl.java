8bb36239ad058ce622b8772fe4a1065591fb362c
najedzony <dominik.komla1@gmail.com>
2022-04-22 14:33:59 +0200

    Provide better solution for turn-handling service.

--- src/main/java/com/nokia/uwr/service/turns/TurnHandlerImpl.java
+++ src/main/java/com/nokia/uwr/service/turns/TurnHandlerImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TurnHandlerImpl implements TurnHandler {
 
-    private static final Logger LOGGER = LogManager.getLogger(Board.class);
+    private static final Logger LOGGER = LogManager.getLogger(TurnHandlerImpl.class);
 
     private final Board board;
     private ScenarioSchema scenario;
@@ -45,32 +45,40 @@ public class TurnHandlerImpl implements TurnHandler {
         /* TODO */
     }
 
+    private void DoAction(UEScenario ueScenario, UEStep ueStep){
+        switch(ueStep.action()){
+            case START:
+                LOGGER.info("Proceeding START action for an UE.");
+                board.addUe(new UE(ueScenario.name()), ueStep.localization());
+                StartCall();
+                LOGGER.info("START action finished.");
+                break;
+            case MOVE:
+                LOGGER.info("Proceeding MOVE action for an UE.");
+                board.updateUe(new UE(ueScenario.name()), ueStep.localization());
+                LOGGER.info("MOVE action finished.");
+                break;
+            case END:
+                try {
+                    LOGGER.info("Proceeding END action for an UE.");
+                    board.deleteUe(new UE(ueScenario.name()));
+                    EndCall();
+                    LOGGER.info("END action finished.");
+                } catch (IllegalArgumentException e) {
+                    LOGGER.error(e.getMessage());
+                }
+                break;
+        }
+    }
+
     @Override
     public void FindAndDoActionsForThisTurn(int TurnNumber) {
         for (UEScenario ueScenario : scenario.ueScenarios()) {
-            for (UEStep ueStep : ueScenario.steps()) {
-                if (ueStep.turn() == TurnNumber) {
-                    if (ueStep.action() == UEAction.START) {
-                        LOGGER.info("Proceeding START action for an UE.");
-                        board.addUe(new UE(ueScenario.name()), ueStep.localization());
-                        StartCall();
-                        LOGGER.info("START action finished.");
-                    } else if (ueStep.action() == UEAction.MOVE) {
-                        LOGGER.info("Proceeding MOVE action for an UE.");
-                        board.updateUe(new UE(ueScenario.name()), ueStep.localization());
-                        LOGGER.info("MOVE action finished.");
-                    } else if (ueStep.action() == UEAction.END) {
-                        try {
-                            LOGGER.info("Proceeding END action for an UE.");
-                            board.deleteUe(new UE(ueScenario.name()));
-                            EndCall();
-                            LOGGER.info("END action finished.");
-                        } catch (IllegalArgumentException e) {
-                            LOGGER.info(e.getMessage());
-                        }
-                    }
+                UEStep ueStep = ueScenario.steps().get(0);
+                if(ueStep.turn() == TurnNumber){
+                    DoAction(ueScenario, ueStep);
+                    ueScenario.steps().remove(0);
                 }
-            }
         }
     }
 
