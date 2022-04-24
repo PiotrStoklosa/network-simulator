27414d665ee8555024c0cea6a3976562df3aea00
BM-ska <barbaramoczulska1a@gmail.com>
2022-04-24 19:13:21 +0200

    Add Javadoc

--- src/main/java/com/nokia/uwr/utility/LocalizationCalculator.java
+++ src/main/java/com/nokia/uwr/utility/LocalizationCalculator.java
@@ -4,14 +4,26 @@ import com.nokia.uwr.board.Localization;
 import lombok.experimental.UtilityClass;
 
 /**
+ * Class responsible for calculations localization
+ * @see Localization
+ *
  * @author Barbara Moczulska
  */
 @UtilityClass
 public class LocalizationCalculator {
 
+
+    /**
+     * Calculate distance between two point
+     *
+     * @param a Localization object
+     * @param b Localization object
+     * @return distance
+     * @author Barbara Moczulska
+     */
     public Integer calculateDistance(Localization a, Localization b) {
 
-        return (int)Math.round(Math.sqrt((b.y() - a.y()) * (b.y() - a.y()) + (b.x() - a.x()) * (b.x() - a.x())));
+        return (int) Math.round(Math.sqrt((b.y() - a.y()) * (b.y() - a.y()) + (b.x() - a.x()) * (b.x() - a.x())));
     }
 
 
